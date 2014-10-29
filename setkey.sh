093d20d72d2ba83386776db9e7c13d5d9821326a
Mark Stillwell <mark@stillwell.me>
2014-10-29 23:54:49 +0000

    typo...

--- setkey.sh
+++ setkey.sh
@@ -9,7 +9,7 @@ while [ ! -f /root/.ssh/authorized_keys ] && [ ${ATTEMPTS} -gt 0 ]; do
   ATTEMPTS=$((${ATTEMPTS}-1))
   curl -f http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key \
     > ${TMPFILE} 2>/dev/null
-  if [ \$? -eq 1 ]; then
+  if [ $? -eq 1 ]; then
     cat ${TMPFILE} >> /root/.ssh/authorized_keys
     chmod 0600 /root/.ssh/authorized_keys
     rm -f ${TMPFILE}
