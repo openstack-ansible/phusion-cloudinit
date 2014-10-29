c29324c8c778cb99441377ea7f4c286e61048c3b
Mark Stillwell <mark@stillwell.me>
2014-10-29 23:42:49 +0000

    moving away from cloud-init for the moment because it seems slow and isn't doing what I want...

---
+++ setkey.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ATTEMPTS=30
+
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
+
+TMPFILE=$(mktemp)
+while [ ! -f /root/.ssh/authorized_keys ] && [ ${ATTEMPTS} -gt 0 ]; do
+  ATTEMPTS=$((${ATTEMPTS}-1))
+  curl -f http://169.254.169.254/latest/meta-data/public-keys/0/openssh-key \
+    > ${TMPFILE} 2>/dev/null
+  if [ \$? -eq 1 ]; then
+    cat ${TMPFILE} >> /root/.ssh/authorized_keys
+    chmod 0600 /root/.ssh/authorized_keys
+    rm -f ${TMPFILE}
+    echo "Successfully retrieved public key from instance metadata"
+    echo "********************************************************"
+    echo "AUTHORIZED KEYS"
+    echo "********************************************************"
+    cat /root/.ssh/authorized_keys
+    echo "********************************************************"
+  fi
+done
